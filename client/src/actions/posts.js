7691a9a690407e0d266dc85c7f3ab79891f626a0
Ben Grinthal <bgrinthal@gmail.com>
2022-08-24 10:26:34 -0400

    posts can now be created in the db

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import * as api from '../api';
+import * as api from '../api/index.js';
 
 //Action creators
 export const getPosts = () => async (dispatch) => {
@@ -6,8 +6,18 @@ export const getPosts = () => async (dispatch) => {
         const { data } = await api.fetchPosts();
 
         //because async dispatch we 'dispatch action' instead of 'return action'
-        dispatch({type: 'FETCH_ALL', payload: data});
+        dispatch({ type: 'FETCH_ALL', payload: data });
     } catch(error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data })
+    } catch (error) {
         console.log(error.message);
     }
 }
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = async (req, res) => {
-    try{
-        const postMessage = await PostMessage.find();
+const router = express.Router();
 
-        res.status(200).json(postMessage)
-    }catch(error){
-        res.status(404).json({ message: error.message })
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -23,3 +28,5 @@ export const createPosts = async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 }
+
+export default router;
